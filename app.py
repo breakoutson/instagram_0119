4bdad388678f489eaa395a08043db22dc560481b
breakoutson <119091441+breakoutson@users.noreply.github.com>
2023-01-26 21:56:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,23 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 import streamlit as st
 
-
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# options = Options()
-# options.add_argument('--disable-gpu')
-# options.add_argument('--headless')
+
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
+driver.get('http://www.instagram.com/accounts/login/)
+
+st.code(driver.page_source)
 
 
 hash_tag = st.text_input("해시태그 입력 >> ")
